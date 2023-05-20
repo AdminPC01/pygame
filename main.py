a60f8c72f1b879866f7e055ff4ca7285acb1c597
Admin <anonymousinko@mail.ru>
2023-05-20 18:57:07 +0300

    First step in studying Pygame

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+import pygame
+from pygame.draw import *
+
+
+pygame.init()
+
+FPS = 30
+screen = pygame.display.set_mode((300, 300))
+
+
+clock = pygame.time.Clock()
+clock.tick(FPS)
+
+rect(screen, (10, 100, 50), (100, 10, 190, 10))
+
+pygame.display.update()
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
 
